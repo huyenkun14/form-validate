4a04fc0e47a4378311ddb6421882bc1c5dd59dbf
huyen khanh <khanhhuyen.tt@secomus.com>
2024-04-03 15:24:22 +0700

    hotfix/validate formik yup

--- src/component/FormikValidation.tsx
+++ src/component/FormikValidation.tsx
@@ -5,10 +5,8 @@ import React from 'react'
 import './form.css'
 
 // ** Third party Imports
-import { useFormik } from 'formik';
-
-// ** Utils Imports
-import { validateEmail, validatePassword } from '../utils/validation';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
 interface IAccount {
   firstName: string
@@ -16,7 +14,7 @@ interface IAccount {
   email: string
   password: string
   rePassword: string
-  checked: any
+  checked: boolean
 }
 
 const FormikValidation: React.FC<{}> = () => {
@@ -31,112 +29,111 @@ const FormikValidation: React.FC<{}> = () => {
     checked: false,
   }
 
-  const validateForm = (values: IAccount) => {
-    if (values.firstName === '' || values.lastName === '' || values.email === '' || values.password === '' || values.rePassword === '') {
-      alert('(formik) Điền đủ các trường!')
-    }
-    else if (!validateEmail(values.email)) {
-      alert('(formik) Email sai định dạng!')
-    }
-    else if (!validatePassword(values.password)) {
-      alert('(formik) Mật khẩu phải có ít nhất 8 kí tự và chứa kí tự đặc biệt!')
-    } else if (values.password !== values.rePassword) {
-      alert('Mật khẩu nhập lại không trùng khớp')
-    }
-    else {
-      alert('(formik) Đăng kí thành công!')
-    }
-  }
-
-  const formik = useFormik({
-    initialValues: initAccount,
-    onSubmit: validateForm
+  const SignupSchema = Yup.object().shape({
+    firstName: Yup.string().required('Required'),
+    lastName: Yup.string().required('Required'),
+    email: Yup.string().email('Invalid email').required('Required'),
+    password: Yup.string().min(8, 'Tối thiểu 8 kí tự!').matches(/[!@#$%^&*]/,'Phải có kí tự đặc biệt!').required('Required'),
+    rePassword: Yup.string().required('Required').oneOf([Yup.ref('password')], 'Mật khẩu nhập lại không trùng khớp!'),
   });
 
   return (
     <div className="register-form">
       <h1>Create your account <span style={{ fontSize: '14px' }}>Formik</span></h1>
       <p>Fill the form below to create an account.</p>
-      <form onSubmit={formik.handleSubmit}>
-        <div className="register-form-row">
-          <div className="register-form-item">
-            <label className="register-form-label">First name*</label>
-            <div className="register-form-input">
-              <input
-                type='text'
-                name='firstName'
-                onChange={formik.handleChange}
-                value={formik.values.firstName}
-              />
+      <Formik
+        initialValues={initAccount}
+        validationSchema={SignupSchema}
+        onSubmit={values => {
+          alert('Đăng kí thành công!');
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            <div className="register-form-row">
+              <div className="register-form-item">
+                <label className="register-form-label">First name*</label>
+                <div className="register-form-input">
+                  <Field
+                    type='text'
+                    name='firstName'
+                  />
+                </div>
+                {errors.firstName && touched.firstName ? (
+                  <div>{errors.firstName}</div>
+                ) : null}
+              </div>
+              <div className="register-form-item">
+                <label className="register-form-label">Last name*</label>
+                <div className="register-form-input">
+                  <Field
+                    type='text'
+                    name='lastName'
+                  />
+                </div>
+                {errors.lastName && touched.lastName ? (
+                  <div>{errors.lastName}</div>
+                ) : null}
+              </div>
             </div>
-          </div>
-          <div className="register-form-item">
-            <label className="register-form-label">Last name*</label>
-            <div className="register-form-input">
-              <input
-                type='text'
-                name='lastName'
-                onChange={formik.handleChange}
-                value={formik.values.lastName}
-              />
+            <div className="register-form-row">
+              <div className="register-form-item">
+                <label className="register-form-label">Email*</label>
+                <div className="register-form-input">
+                  <Field
+                    type='text'
+                    name='email'
+                  />
+                </div>
+                {errors.email && touched.email ? (
+                  <div>{errors.email}</div>
+                ) : null}
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="register-form-row">
-          <div className="register-form-item">
-            <label className="register-form-label">Email*</label>
-            <div className="register-form-input">
-              <input
-                type='text'
-                name='email'
-                onChange={formik.handleChange}
-                value={formik.values.email}
-              />
+            <div className="register-form-row">
+              <div className="register-form-item">
+                <label className="register-form-label">Password*</label>
+                <div className="register-form-input">
+                  <Field
+                    type='password'
+                    name='password'
+                  />
+                </div>
+                {errors.password && touched.password ? (
+                  <div>{errors.password}</div>
+                ) : null}
+              </div>
+              <div className="register-form-item">
+                <label className="register-form-label">Password confimation*</label>
+                <div className="register-form-input">
+                  <Field
+                    type='password'
+                    name='rePassword'
+                  />
+                </div>
+                {errors.rePassword && touched.rePassword ? (
+                  <div>{errors.rePassword}</div>
+                ) : null}
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="register-form-row">
-          <div className="register-form-item">
-            <label className="register-form-label">Password*</label>
-            <div className="register-form-input">
-              <input
-                type='password'
-                name='password'
-                onChange={formik.handleChange}
-                value={formik.values.password}
-              />
+            <div className="register-params">
+              <p>We want you to know exactly how our service works and why we need your details.</p>
+              <p>Please confirm that you have read, understood and accept the terms and conditions.</p>
             </div>
-          </div>
-          <div className="register-form-item">
-            <label className="register-form-label">Password confimation*</label>
-            <div className="register-form-input">
-              <input
-                type='password'
-                name='rePassword'
-                onChange={formik.handleChange}
-                value={formik.values.rePassword}
+            <div className="register-form-row">
+              <Field
+                type="checkbox"
+                name="checked"
+                id="checked"
               />
+              <p>I have read, understood and accept the terms and conditions.</p>
             </div>
-          </div>
-        </div>
-        <div className="register-params">
-          <p>We want you to know exactly how our service works and why we need your details.</p>
-          <p>Please confirm that you have read, understood and accept the terms and conditions.</p>
-        </div>
-        <div className="register-form-row">
-          <input
-            type="checkbox"
-            name="checked"
-            id="checked"
-            onChange={formik.handleChange}
-            value={formik.values.checked}
-          />
-          <p>I have read, understood and accept the terms and conditions.</p>
-        </div>
-        <button id="register-form-button" type='submit'>
-          Next
-        </button>
-      </form>
+            <button id="register-form-button" type='submit'>
+              Next
+            </button>
+          </Form>
+        )}
+      </Formik>
     </div>
   )
 }
